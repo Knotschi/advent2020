1e576593559e37aaa9ab5fad4835c0b0df1e87d7
Damiano Ruehl <damiano.ruehl@redpineapplemedia.com>
2020-12-16 13:27:52 +0100

    alternative tribonacci solution for day ten

--- src/advent/ten.clj
+++ src/advent/ten.clj
@@ -91,3 +91,45 @@
 
   )
 
+
+
+;; alternative O(n) solution for second quiz:
+;; considering that we actually never have gaps of 2 jolts between adapters
+;; but only gaps of 1 or 3 jolts
+;; we can also use the tribonacci sequence to calculate the distinct combinations
+;; why can we do that? I have no clue. It was just a observation I made.
+;; I am sure there is a smart logical mathematical reason for that.
+
+;; the first part of this solution
+;; which is changing the list of adapter jolts to a list of jolt difference to its previous adapter
+;; from here: https://www.youtube.com/watch?v=1CPuvWvJMFo
+
+;; the tribonacci sequence
+;; although we could use a function to calculate them to als support higher values
+;; but we actually only need pretty low tribonacci numbers for our quiz, so I just hardcode the first ones:
+(def trib [1, 1, 2, 4, 7, 13, 24, 44, 81, 149])
+
+(defn count-options-trib
+  [jolts]                                                   ; [1 4 5 8 11 12 13 14 17 18 19 20]
+       ; prepend the outlet (adapter with 0 jolts):
+  (->> (into [0] jolts)                                     ; [0 1 4 5 8 11 12 13 14 17 18 19 20]
+       ; for each adapter we need the jolts difference to its previous adapter:
+       (partition 2 1)                                      ; ((0 1) (1 4) (4 5) (5 8) (8 11) (11 12) (12 13) (13 14) (14 17) (17 18) (18 19) (19 20) (20 23))
+       (mapv #(->> % reverse (apply -)))                    ; [1 3 1 3 3 1 1 1 3 1 1 1 3]
+       ; replacing the 3s with 0s because a difference of 3 means that adapter does not add any new configuration options:
+       (mapv {3 0 1 1})                                     ; [1 0 1 0 0 1 1 1 0 1 1 1 0]
+       ; grouping by same values:
+       (partition-by identity)                              ; ((1) (0) (1) (0 0) (1 1 1) (0) (1 1 1) (0))
+       ; replacing the groups of 0s with 1 and
+       ; replacing each group of 1s with the tribonacci number of its count:
+       (mapv #(trib (apply + %)))                           ; [1 1 1 1 4 1 4 1]
+       ; multiplicate all these values:
+       (reduce *)))                                         ; 16
+
+(comment
+  (count-options-trib [1 4 5 8 11 12 13 14 17 18 19 20])
+  ; => 16
+  (count-options-trib (read-file "resources/ten.data"))
+  ; => 2314037239808
+
+  )
